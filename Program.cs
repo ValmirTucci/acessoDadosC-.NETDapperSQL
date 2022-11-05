78112d55ba270d5d90534ea30c6854544bd5cec2
ValmirTucci <valmirizzo@gmail.com>
2022-11-05 19:30:13 -0300

    Criando conexão de acesso ao BD

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace acessoDadosC.NETDapperSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "localhost,1433;Database=tucci;User ID=sa;Password=1q2w3e4r@#$"; //String de conexão com o servidor do banco de dados
+            //Microsoft.Data.SqlClient -> Pacote de acesso
+            Console.WriteLine("Hello World!");
+        }
+
+        //Integrated Security = SSPI
+    }
+}

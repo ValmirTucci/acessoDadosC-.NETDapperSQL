cbb9797c8fcc8cc6edcd7034ca65bf32d3e2d9b2
ValmirTucci <valmirizzo@gmail.com>
2022-11-16 20:48:19 -0300

    Executando procedures

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using Dapper;
 using acessoDadosC.NETDapperSQL.Models;
+using System.Data;
 
 namespace acessoDadosC.NETDapperSQL
 {
@@ -41,8 +42,10 @@ namespace acessoDadosC.NETDapperSQL
 
             using (var connection = new SqlConnection(connectionString)) //Método de conexão optimizado
             {
-                UpdateCategory(connection);
+                //UpdateCategory(connection);
+                //CreateManyCategory(connection);
                 ListCategories(connection);
+                ExecuteProcedure(connection);
                 //CreateCategory(connection);
 
                 /*var rows = connection.Execute(insertSql, new
@@ -88,16 +91,16 @@ namespace acessoDadosC.NETDapperSQL
 
         //Integrated Security = SSPI
 
-        static void ListCategories (SqlConnection connection)
+        static void ListCategories(SqlConnection connection)
         {
             var categories = connection.Query<Category>("SELECT [Id], [Title] AS FROM [Category]");
-                foreach (var item in categories)
-                {
-                    Console.WriteLine($"{item.Id} - {item.Title}");
-                }
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
         }
 
-        static void CreateCategory (SqlConnection connection)
+        /*static void CreateCategory(SqlConnection connection)
         {
             var category = new Category();
             category.Id = Guid.NewGuid();
@@ -132,10 +135,10 @@ namespace acessoDadosC.NETDapperSQL
                     category.Featured
                 });
                 Console.WriteLine($"{rows} linhas inseridas");
-        }
-        }
+            }
+        }*/
 
-        static void UpdateCategory (SqlConnection connection)
+        static void UpdateCategory(SqlConnection connection)
         {
             var updateQuery = "UPDATE [Category] SET [Title] = @title WHERE [Id] = @id";
             var rows = connection.Execute(updateQuery, new
@@ -145,6 +148,73 @@ namespace acessoDadosC.NETDapperSQL
             });
 
             Console.WriteLine($"{rows} registros atualizados");
+        }
+
+        static void CreateManyCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var category2 = new Category();
+            category2.Id = Guid.NewGuid();
+            category2.Title = "Categoria nova";
+            category2.Url = "Categoria nova";
+            category2.Description = "Categoria nova";
+            category2.Order = 9;
+            category2.Summary = "Categoria";
+            category2.Featured = true;
+
+            var insertSql = @$"INSERT INTO
+                    [Category] 
+                    VALUES(
+                        @Id, 
+                        @Title, 
+                        @Url, 
+                        @Summary, 
+                        @Order, 
+                        @Description, 
+                        @Featured)";
+            {
+
+                var rows = connection.Execute(insertSql, new[]{
+                new
+                {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                },
+                new
+                {
+                    category2.Id,
+                    category2.Title,
+                    category2.Url,
+                    category2.Summary,
+                    category2.Order,
+                    category2.Description,
+                    category2.Featured
+                }
+            });
+                Console.WriteLine($"{rows} linhas inseridas");
+            }
+        }
+
+        static void ExecuteProcedure(SqlConnection connection){
+            var procedure = "spDeleteStudent";
+            var pars = new {StudentId="6bd552ea-7187-4bae-abb6-54e8f8b9f530"};
+
+            var affectedRows = connection.Execute(procedure, pars, commandType: CommandType.StoredProcedure);
+
+            Console.WriteLine($"{affectedRows} linhas afetadas");
         }
     }
 }

13f4041b1c733acad7e8b82dedc1bf9196f95950
ValmirTucci <valmirizzo@gmail.com>
2022-11-16 21:10:09 -0300

    Execute Scalar

--- Program.cs
+++ Program.cs
@@ -47,7 +47,8 @@ namespace acessoDadosC.NETDapperSQL
                 //ListCategories(connection);
                 //ExecuteProcedure(connection);
                 //CreateCategory(connection);
-                ExecuteReadProcedure(connection);
+                //ExecuteReadProcedure(connection);
+                ExecuteScalar(connection);
 
                 /*var rows = connection.Execute(insertSql, new
                 {
@@ -218,7 +219,6 @@ namespace acessoDadosC.NETDapperSQL
 
             Console.WriteLine($"{affectedRows} linhas afetadas");
         }
-
         static void ExecuteReadProcedure(SqlConnection connection)
         {
             var procedure = "spGetCoursesByCategory";
@@ -230,6 +230,45 @@ namespace acessoDadosC.NETDapperSQL
             {
                 Console.WriteLine(item.Title);
 
+            }
+        }
+
+        static void ExecuteScalar(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+
+            var insertSql = @"
+            INSERT INTO
+                    [Category] 
+                    OUTPUT inserted.[Id]
+                    VALUES(
+                        NEWID(), 
+                        @Title, 
+                        @Url, 
+                        @Summary, 
+                        @Order, 
+                        @Description, 
+                        @Featured) 
+                        SELECT SCOPE_IDENTITY()";
+            {
+
+                var id = connection.ExecuteScalar<Guid>(insertSql, new
+                {   
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                Console.WriteLine($"A categoria inserida foi: {id} linhas.");
             }
         }
     }
